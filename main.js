c227a5729798490ad9dc8fff3e834a634f669a48
nicoler6655 <123043065+nicoler6655@users.noreply.github.com>
2023-01-30 17:01:17 -0500

    ifinal# Please enter the commit message for your changes. Lines starting

---
+++ main.js
@@ -0,0 +1,10 @@
+function buttonSelected() {
+	console.log("button clicked");
+
+	let newText = "Number of times clickedL" +1;
+
+	let buttonDiv = document.getElementById("button-div");
+
+	buttonDiv.innerHTMLnewText;
+	
+}
